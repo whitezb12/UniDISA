dcce52bc79ad53abd39a9f0c837cec7dec3ced8c
FDU_zflu <109411088+whitezb12@users.noreply.github.com>
2026-05-22 18:07:34 +0800

    delete

--- AsymAE/__init__.py
+++
@@ -1,8 +0,0 @@
-import SymAE.model
-import SymAE.network
-import SymAE.utils
-import SymAE.smooth
-from .model import Model
-from .smooth import smooth_link_feat
-
-__version__ = '1.0.0'
--- mycode/__init__.py
+++
@@ -1,9 +0,0 @@
-import mycode.model
-import mycode.network
-import mycode.utils
-import mycode.smooth
-import mycode.metrics
-from .model import Model
-from .smooth import smooth_link_feat
-
-__version__ = '1.0.0'
